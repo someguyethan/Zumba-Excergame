65a4248c9b7f778914c06acd48ddf760ccd23e83
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2023-01-19 12:53:49 -0500

    Working on Matching Game and Waldo

---
+++ Assets/Scripts/MatchingGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchingGame : MonoBehaviour
+{
+    public int count;
+    public Text card1;
+    public Text card2;
+    public bool locked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 2)
+        {
+            if (card1 == card2)
+            {
+                locked = true;
+            }
+            count = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Waldo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Waldo : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        if (pointerEventData.button == PointerEventData.InputButton.Left)
+        {
+            //do this when clicked
+        }
+    }
+}
